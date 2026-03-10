21d1c3d844e30707efa336be9d318217d55813a7
advancegamessp-collab <advancegamessp@gmail.com>
2026-03-10 20:15:55 +0000

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,8 +39,8 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363348739987203@newsletter',
-                newsletterName: config.OWNER_NAME,
+                newsletterJid: '120363423818692647@newsletter',
+                newsletterName: config.Eliuba,
                 serverMessageId: 143
             }
         };
