3ab1c711b9f511de8f6ec8adaeb2cd22170dd7e2
advancegamessp-collab <advancegamessp@gmail.com>
2026-03-10 20:21:24 +0000

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -41,8 +41,8 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: "𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃",
+                    newsletterJid: '120363423818692647@newsletter',
+                    newsletterName: "E-liubaBot",
                     serverMessageId: 143
                 }
             }
